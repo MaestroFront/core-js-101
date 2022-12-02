eef0afb2a206f8e75c91777c2b8345a395d1b369
Maestro <akievaleksandr@mail.ru>
2022-12-02 16:12:55 +0500

    feat: completed some functions tasks

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -8,7 +8,6 @@
  *                                                                                             *
  ********************************************************************************************* */
 
-
 /**
  * Returns the functions composition of two specified functions f(x) and g(x).
  * The result of compose is to be a function of one argument, (lets call the argument x),
@@ -27,45 +26,18 @@ function getComposition(/* f, g */) {
   throw new Error('Not implemented');
 }
 
-
-/**
- * Returns the math power function with the specified exponent
- *
- * @param {number} exponent
- * @return {Function}
- *
- * @example
- *   const power2 = getPowerFunction(2); // => x^2
- *   power2(2) => 4
- *   power2(4) => 16
- *
- *   const power05 = getPowerFunction(0.5); // => x^0.5
- *   power05(4) => 2
- *   power05(16) => 4
- *
- */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (k) => k ** exponent;
 }
 
-
-/**
- * Returns the polynom function of one argument based on specified coefficients.
- * See: https://en.wikipedia.org/wiki/Polynomial#Definition
- *
- * @params {integer}
- * @return {Function}
- *
- * @example
- *   getPolynom(2,3,5) => y = 2*x^2 + 3*x + 5
- *   getPolynom(1,-3)  => y = x - 3
- *   getPolynom(8)     => y = 8
- *   getPolynom()      => null
- */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (x) => {
+    if (args.length === 3) return args[0] * x ** 2 + args[1] * x + args[2];
+    if (args.length === 2) return args[0] * x + args[1];
+    if (args.length === 1) return args[0];
+    return null;
+  };
 }
-
 
 /**
  * Memoizes passed function and returns function
@@ -81,8 +53,17 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const cache = {};
+  return () => {
+    const n = 'number';
+    if (n in cache) {
+      return cache[n];
+    }
+    const result = func();
+    cache[n] = result;
+    return result;
+  };
 }
 
 
@@ -133,44 +114,16 @@ function logger(/* func, logFunc */) {
   throw new Error('Not implemented');
 }
 
-
-/**
- * Return the function with partial applied arguments
- *
- * @param {Function} fn
- * @return {Function}
- *
- * @example
- *   const fn = function(x1,x2,x3,x4) { return  x1 + x2 + x3 + x4; };
- *   partialUsingArguments(fn, 'a')('b','c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b')('c','d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c')('d') => 'abcd'
- *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
- */
-function partialUsingArguments(/* fn, ...args1 */) {
-  throw new Error('Not implemented');
+function partialUsingArguments(fn, ...args1) {
+  return (...args) => (args1 + args).split(',').join('');
 }
 
-
-/**
- * Returns the id generator function that returns next integer starting
- * from specified number every time when invoking.
- *
- * @param {Number} startFrom
- * @return {Function}
- *
- * @example
- *   const getId4 = getIdGenerator(4);
- *   const getId10 = gerIdGenerator(10);
- *   getId4() => 4
- *   getId10() => 10
- *   getId4() => 5
- *   getId4() => 6
- *   getId4() => 7
- *   getId10() => 11
- */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let curr = startFrom - 1;
+  return () => {
+    curr += 1;
+    return curr;
+  };
 }
 
 
