f3eb5833265704472d869e9af567857299873fb8
Maestro <akievaleksandr@mail.ru>
2022-12-27 22:05:36 +0500

    feat: done some functions-task

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -1,29 +1,5 @@
-/* *********************************************************************************************
- *                                                                                             *
- * Please read the following tutorial before implementing tasks:                                *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions                     *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function   *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/arguments       *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures                            *
- *                                                                                             *
- ********************************************************************************************* */
-
-/**
- * Returns the functions composition of two specified functions f(x) and g(x).
- * The result of compose is to be a function of one argument, (lets call the argument x),
- * which works like applying function f to the result of applying function g to x, i.e.
- *  getComposition(f,g)(x) = f(g(x))
- *
- * @param {Function} f
- * @param {Function} g
- * @return {Function}
- *
- * @example
- *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
- *
- */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (...args) => f(g(...args));
 }
 
 function getPowerFunction(exponent) {
