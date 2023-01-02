af8f27648423357e0d3f981275f0b69ff8865926
Maestro <akievaleksandr@mail.ru>
2023-01-03 02:42:34 +0500

    feat: done one task on loops file

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -58,8 +58,11 @@ function doRectanglesOverlap(rect1, rect2) {
  *   { center: { x:0, y:0 }, radius:10 },  { x:10, y:10 }   => false
  *
  */
-function isInsideCircle(/* circle, point */) {
-  throw new Error('Not implemented');
+function isInsideCircle(circle, point) {
+  const a = (point.x - circle.center.x) ** 2;
+  const b = (point.y - circle.center.y) ** 2;
+  const c = Math.sqrt(a + b);
+  return c < circle.radius;
 }
 
 function findFirstSingleChar(str) {
